480b483daf2fca465e0afe946b70b743a5136eb2
0910QiChen <0910qichen@gmail.com>
2024-06-27 21:18:10 -0400

    Add project files.

---
+++ MVCProject/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCProject.DAL;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class UserController : Controller
+    {
+        UserDAL userDAL = new UserDAL();
+
+        public ActionResult Signup()
+        {
+            return View("Signup");
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User user)
+        {
+            if (userDAL.IsEmailExists(user.email) == true)
+            {
+                ModelState.AddModelError("email", "Email address already exists!");
+            }
+            else if (userDAL.IsUsernameExists(user.username) == true)
+            {
+                ModelState.AddModelError("username", "Username already exists!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            userDAL.NewUser(user);
+            return RedirectToAction("UserList");
+        }
+
+        [HttpGet]
+        public ActionResult UserList()
+        {
+            List<User> user = userDAL.GetUsers();
+            return View(user);
+        }
+    }
+}
---
+++ MVCProject/DAL/UserDAL.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Web.Helpers;
+using MVCProject.Security;
+using MVCProject.Models;
+using System.Web.ModelBinding;
+
+namespace MVCProject.DAL
+{
+    public class UserDAL
+    {
+        private string ConnStr;
+        public UserDAL()
+        {
+            ConnStr = ConfigurationManager.ConnectionStrings["MyDBConnStr"].ConnectionString;
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> userList = new List<User>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnStr))
+                {
+                    string query = "SELECT UserID, UserName, Email FROM Users";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            User user = new User
+                            {
+                                userid = Convert.ToInt32(reader["UserID"]),
+                                username = Convert.ToString(reader["Username"]),
+                                email = Convert.ToString(reader["Email"])
+                            };
+                            userList.Add(user);
+                        }
+                    }
+                }
+            } catch (SqlException e)
+            {
+                Console.WriteLine("SQL Error Occurred: " + e.Message);
+                throw;
+            } catch (Exception e)
+            {
+                Console.WriteLine("Error Occurred: " + e.Message);
+                throw;
+            }
+            return userList;
+        }
+
+        public void NewUser(User user)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnStr))
+            {
+                try
+                {
+                    string query = "INSERT INTO Users (UserName, Password, Email) VALUES (@Username, @HashedPassword, @VerifiedEmail)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        string hashedPassword = PasswordSecurity.HashPassword(user.password);
+                        cmd.Parameters.AddWithValue("@Username", user.username);
+                        cmd.Parameters.AddWithValue("@HashedPassword", hashedPassword);
+                        cmd.Parameters.AddWithValue("@VerifiedEmail", user.email);
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                } catch (SqlException e)
+                {
+                    Console.WriteLine("SQL Error Occurred: " + e.Message);
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error Occurred: " + e.Message);
+                    throw;
+                }
+            }
+        }
+
+        public bool IsEmailExists(string email)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnStr))
+            {
+                try
+                {
+                    string query = "SELECT COUNT(*) FROM Users WHERE Email = @Email";
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Email", email);
+                        int count = (int)cmd.ExecuteScalar();
+                        return count > 0;
+                    }
+                } catch (SqlException e)
+                {
+                    Console.WriteLine("SQL Error Occurred: " + e.Message);
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error Occurred: " + e.Message);
+                    throw;
+                }
+            }
+        }
+
+        public bool IsUsernameExists(string username)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnStr))
+            {
+                try {
+                    string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username";
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        int count = (int)cmd.ExecuteScalar();
+                        return count > 0;
+                    }
+                } catch (SqlException e)
+                    {
+                    Console.WriteLine("SQL Error Occurred: " + e.Message);
+                    throw;
+                }
+                    catch (Exception e)
+                    {
+                    Console.WriteLine("Error Occurred: " + e.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
